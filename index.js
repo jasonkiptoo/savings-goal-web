34340102297c2fe10573f8725c04432ba888c17b
jasonkiptoo <kiptoojason@gmail.com>
2022-09-07 20:35:30 +0300

    added the slideshow feature

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ document.addEventListener('DOMContentLoaded',()=>{
 		//  console.log(e.target.targetAmount.value)
 	});
 
-
 	const submitBtn=document.querySelector("#submitBtn");
-
 	submitBtn.addEventListener("click",()=>{
 		calcSavings()
-
 	});
 	
-
+//method to calculate how much to save per month
 	function calcSavings(){
 		const rate=0.1;
 		let targetDuration=parseInt(document.getElementById("targetDuration").value)
@@ -24,27 +21,17 @@ document.addEventListener('DOMContentLoaded',()=>{
 		displayAmount(totalSavings,targetDuration)
 
 	}
-
+/////function to display the target amount per month.
 	function displayAmount(totalSavings,targetDuration){
 		const displayValue=document.getElementById("display-values")
 		// let amount=totalSavings;
+		// document.getElementById("display-Values-Form").style.border = "thick solid red";
 		const amountSaved=document.getElementById('disp-Amount');
-		amountSaved.textContent=`Pay ${totalSavings} Ksh every month for the next ${targetDuration} years to achieve your goal`
+		amountSaved.textContent=`Pay ${totalSavings} Ksh every month for the next ${targetDuration} year(s) to achieve your goal !`
 		displayValue.appendChild(amountSaved)
-
-
 	}
-
-	
-
-
-
 	})
-   
-
-
-
-
+  
 
 
 // const options = {
@@ -60,31 +47,40 @@ document.addEventListener('DOMContentLoaded',()=>{
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '69777ab306msh505ca15cae9f198p15c351jsn20363828244e',
-// 		'X-RapidAPI-Host': 'all-cars.p.rapidapi.com'
-// 	}
-// };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '69777ab306msh505ca15cae9f198p15c351jsn20363828244e',
+		'X-RapidAPI-Host': 'car-database.p.rapidapi.com'
+	}
+};
 
-// fetch('https://all-cars.p.rapidapi.com/cars', options)
-// 	.then(response => response.json())
-// 	.then(function(data){
-		
-// 		console.log(data)
-    
+fetch('https://car-database.p.rapidapi.com/makes', options)
+	.then(response => response.json())
+	.then(function(data){
+		console.log(data)	
+		const make=document.querySelector("#ul-id")
+    data.slice(0, 10).forEach(makes => {
+       const li=document.createElement('h4')
+        li.innerHTML=makes.name  
+        make.appendChild(li)
+    })
 
-// const make=document.querySelector("#ul-id")
+	//code to fetch imagees on to the card
+	// Change image every 2 seconds
+	let slideIndex = 0;
+	showSlides();
 
-//     data.forEach(makes => {
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); 
+}
 
-// 		// makes.forEach(car=>{
-
-// 		// })
-//         const li=document.createElement('h4')
-//         li.innerHTML=makes.title  
-//         make.appendChild(li)
-//     })
-
-// })   
+})   
